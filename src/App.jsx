6d86dffbb9e8a55493b1404927a9ce26f2805426
tawhid <ahmedshanto690@gmail.com>
2025-07-11 14:51:26 +0600

    basic project settup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
